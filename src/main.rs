5ac6aebd64901037d4783fc6326879a73a1b1b07
catherine4331 <catherine4331@gmail.com>
2024-02-15 20:33:59 +0000

    Scale the plot to the input size and calculate the 1D diffusion equation via finite difference methods

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,19 @@ fn main() {
     let dx = 2.0 / (nx as f64 - 1.0); // Distance between points
     let timesteps = 25; // Total timesteps
     let dt = 0.025; // Timestep time delta
-    let c = 1; // Wave speed
+    let c = 1.0; // Wave speed
 
     // Initial condition grid
     let mut u = vec![1.0; nx];
     u[(0.5 / dx) as usize..(1.0 / dx) as usize].fill(2.0);
+
+    // Run the linear convection method. We are using forward difference for the time derivative and backward difference for the space derivative
+    for _ in 0..timesteps {
+        let un = u.clone();
+        for i in 1..nx {
+            u[i] = un[i] - c * dt / dx * (un[i] - un[i - 1])
+        }
+    }
 
     plot(&u);
 }
@@ -29,9 +37,11 @@ fn plot(data: &Vec<f64>) {
         .build_cartesian_2d(0.0_f64..2.0, 0.8_f64..2.2)
         .unwrap();
 
+    let dx = 2.0 / data.len() as f64;
+
     chart
         .draw_series(LineSeries::new(
-            data.iter().enumerate().map(|(i, x)| (i as f64, *x)),
+            data.iter().enumerate().map(|(i, u)| (i as f64 * dx, *u)),
             &RED,
         ))
         .unwrap();
