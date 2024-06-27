7899a6298642100f6108f439093c6404a674e12e
catherine4331 <catherine4331@gmail.com>
2024-06-27 23:53:08 +0100

    Nonlinear convection & starting diffusion

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,68 @@ use plotters::{
     style::{RED, WHITE},
 };
 
-fn main() {
-    let nx = 41; // Number of grid points
-    let dx = 2.0 / (nx as f64 - 1.0); // Distance between points
-    let timesteps = 25; // Total timesteps
-    let dt = 0.025; // Timestep time delta
-    let c = 1.0; // Wave speed
+const GRID_WIDTH: f64 = 2.0;
 
-    // Initial condition grid
-    let mut u = vec![1.0; nx];
-    u[(0.5 / dx) as usize..(1.0 / dx) as usize].fill(2.0);
+#[derive(Debug)]
+struct RunConfig {
+    nx: usize,      // Number of grid points
+    dx: f64,        // Distance between points
+    timesteps: i32, // Total timesteps
+    dt: f64,        // Timestep time delta
+}
 
-    // Run the linear convection method. We are using forward difference for the time derivative and backward difference for the space derivative
-    for _ in 0..timesteps {
-        let un = u.clone();
-        for i in 1..nx {
-            u[i] = un[i] - c * dt / dx * (un[i] - un[i - 1])
+impl RunConfig {
+    fn new(nx: usize, total_distance: f64, timesteps: i32, dt: f64) -> Self {
+        Self {
+            nx: nx,
+            dx: total_distance / (nx as f64 - 1.0),
+            timesteps: timesteps,
+            dt: dt,
         }
     }
+}
+
+fn main() {
+    let conf = RunConfig::new(101, GRID_WIDTH, 600, 0.001);
+
+    println!("{:?}", conf);
+    // Initial condition grid
+    let mut u = vec![1.0; conf.nx];
+    u[(0.5 / conf.dx) as usize..(1.0 / conf.dx) as usize].fill(2.0);
+
+    nonlinear_convection(&conf, &mut u);
 
     plot(&u);
+}
+
+// 1D Diffusion. We use central difference for the second derivative and forward difference for time.
+fn diffusion(conf: &RunConfig, n: &mut Vec<f64>) {
+    for _ in 0..conf.timesteps {
+        let un = n.clone();
+        for i in 1..conf.nx {
+            n[i] = todo!();
+        }
+    }
+}
+
+// 1D nonlinear convection. The wavespeed is now u rather than being fixed. The same finite difference methods are used as for linear convection
+fn nonlinear_convection(conf: &RunConfig, n: &mut Vec<f64>) {
+    for _ in 0..conf.timesteps {
+        let un = n.clone();
+        for i in 1..conf.nx {
+            n[i] = un[i] - un[i] * conf.dt / conf.dx * (un[i] - un[i - 1]);
+        }
+    }
+}
+
+// 1D linear convection. We are using forward difference for the time derivative and backward difference for the space derivative
+fn linear_convection(conf: &RunConfig, c: f64, n: &mut Vec<f64>) {
+    for _ in 0..conf.timesteps {
+        let un = n.clone();
+        for i in 1..conf.nx {
+            n[i] = un[i] - c * conf.dt / conf.dx * (un[i] - un[i - 1])
+        }
+    }
 }
 
 fn plot(data: &Vec<f64>) {
@@ -37,7 +79,7 @@ fn plot(data: &Vec<f64>) {
         .build_cartesian_2d(0.0_f64..2.0, 0.8_f64..2.2)
         .unwrap();
 
-    let dx = 2.0 / data.len() as f64;
+    let dx = GRID_WIDTH / data.len() as f64;
 
     chart
         .draw_series(LineSeries::new(
